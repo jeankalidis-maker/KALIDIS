a838fa77f8078f2cd82dc3a9c65a5c25b684a92c
jeankalidis-maker <jeankalidis@gmail.com>
2026-08-19 16:12:52 -0300

    Process local bridge commands during Revit idling

--- RevitBridge/Kalidis.Revit/Application.cs
+++ RevitBridge/Kalidis.Revit/Application.cs
@@ -1,5 +1,6 @@
 using Autodesk.Revit.DB.Events;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
 
 namespace Kalidis.Revit;
 
@@ -8,24 +9,41 @@ public class Application : IExternalApplication
     public Result OnStartup(UIControlledApplication application)
     {
         application.ControlledApplication.DocumentOpened += OnDocumentOpened;
+        application.Idling += OnIdling;
+        BridgeService.EnsureFiles();
         return Result.Succeeded;
     }
 
     public Result OnShutdown(UIControlledApplication application)
     {
         application.ControlledApplication.DocumentOpened -= OnDocumentOpened;
+        application.Idling -= OnIdling;
         return Result.Succeeded;
     }
 
-    private static void OnDocumentOpened(object sender, DocumentOpenedEventArgs e)
+    private static void OnDocumentOpened(object? sender, DocumentOpenedEventArgs e)
     {
         try
         {
             InventoryService.Generate(e.Document);
+            BridgeService.EnsureFiles();
         }
         catch
         {
-            // Nunca bloquear a abertura do Revit por falha no inventário automático.
+            // Nunca bloquear a abertura do Revit por falha no inventário/bridge.
+        }
+    }
+
+    private static void OnIdling(object? sender, IdlingEventArgs e)
+    {
+        try
+        {
+            if (sender is UIApplication uiApp)
+                BridgeService.TryProcess(uiApp);
+        }
+        catch
+        {
+            // Bridge não pode travar a interface do Revit.
         }
     }
 }
